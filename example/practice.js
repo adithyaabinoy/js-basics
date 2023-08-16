43a72f52f25be76aa8fc75800bc4b62e8e985681
adithyaabinoy <adithyaabinoy@gmail.com>
2023-08-16 12:30:53 +0530

    js basics

---
+++ example/practice.js
@@ -0,0 +1,68 @@
+//fibbonaci series
+
+/* let numberOne = 0;
+let nextNumber = 1;
+let sum;
+for(let i =0; i<10; i++) {
+sum = numberOne + nextNumber;
+numberOne = nextNumber 
+nextNumber = sum
+console.log(nextNumber);
+} */
+
+
+
+/* let string = "Welcome to this Javascript Guide!";
+let reverseString = "";
+let arr = string.split(" ")
+for(let i=0; i<arr.length; i++){
+arr[i] = (arr[i].split("").reverse().join(""))
+}
+console.log(arr.join(" "))
+ */
+ 
+ 
+ 
+//print 1 to 5 in reverse manner
+/* 
+for(let i=5; i>=1; i--) {
+console.log(i)
+}
+
+for(let i =1; i<=5; i++) {
+console.log(i)
+}
+ */
+ 
+ 
+ //array : return sum of it
+ 
+ /* let array = [1,2,3,4,5,6];
+ let sum = 0;
+ for(let i=0; i<array.length; i++) {
+ sum = sum + array[i];
+  console.log(sum)
+ } */
+
+
+
+//No array and each number should be multipied by 2
+
+/* let array = [1,2,3,4,5,6,7];
+for(let i =0; i<array.length; i++) {
+array[i] = array[i] * 2;
+}
+console.log(array)
+  */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
