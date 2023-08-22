9c18a8d27228819efdf10c0b09739eedebd4a9c8
adithyaabinoy <adithyaabinoy@gmail.com>
2023-08-22 11:51:32 +0530

    js_basics

--- example/hof.js
+++ example/hof.js
@@ -31,3 +31,74 @@
   let result=sum(array)
    console.log(result);
   */
+
+
+  //find how many ppl has voted 
+/* function totalVotes(arr) {
+    let votes = arr.reduce((a, b) => {
+      if (b.voted) {
+        return a + 1;
+      } else {
+        return a;
+      }
+    }, 0)
+    return votes;
+  }
+  
+  var voters = [{
+      name: 'Bob',
+      age: 30,
+      voted: true
+    },
+    {
+      name: 'Jake',
+      age: 32,
+      voted: true
+    },
+    {
+      name: 'Kate',
+      age: 25,
+      voted: false
+    },
+    {
+      name: 'Sam',
+      age: 20,
+      voted: false
+    },
+    {
+      name: 'Phil',
+      age: 21,
+      voted: true
+    },
+    {
+      name: 'Ed',
+      age: 55,
+      voted: true
+    },
+    {
+      name: 'Tami',
+      age: 54,
+      voted: true
+    },
+    {
+      name: 'Mary',
+      age: 31,
+      voted: false
+    },
+    {
+      name: 'Becky',
+      age: 43,
+      voted: false
+    },
+    {
+      name: 'Joey',
+      age: 41,
+      voted: true
+    },
+    {
+      name: 'Zack',
+      age: 19,
+      voted: false
+    }
+  ];
+  console.log(totalVotes(voters)); // 6 */
