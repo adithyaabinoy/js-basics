e97090450834bd41a10c6674be8cd4aa52fc3bcb
adithyaabinoy <adithyaabinoy@gmail.com>
2023-08-22 11:49:33 +0530

    js_basics

---
+++ example/hof.js
@@ -0,0 +1,33 @@
+//print sum of even no's using reduce function
+  
+  /* let array = [1,2,3,4,5,6,7,8,9];
+  function sum(arr) {
+  let evenSum  = arr.reduce((a,b) => {
+  if(b % 2 == 0) {
+  return a + b;
+  } else {
+  return a ;
+  }
+  },0)
+  return evenSum;
+  }
+  let result = sum(array);
+  console.log(result); */
+  
+  
+  //print sum of odd numbers
+
+  /*  let array = [1,2,3,4,5];
+  function sum(arr) {
+  let c = arr.reduce((a,b) => {
+  if(b % 2 !== 0) {
+  return a+b
+  } else {
+  return a;
+  }
+  },0)
+  return c;
+  }
+  let result=sum(array)
+   console.log(result);
+  */
